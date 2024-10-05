3f91021543f6abd9639a388424ff52489d2c77a5
AasifNazir786 <aasif.n.s786@gmail.com>
2024-10-05 16:25:14 +0530

    created view pages for project model

--- MVC-SpringBoot/src/main/java/com/example/MVC_SpringBoot/controller/ProjectController.java
+++ MVC-SpringBoot/src/main/java/com/example/MVC_SpringBoot/controller/ProjectController.java
@@ -30,33 +30,33 @@ public class ProjectController {
     }
 
     @PostMapping
-    public String createProject(@ModelAttribute("project")Project project, Model model){
-        model.addAttribute("project", project);
-        return "redirect: projects";
+    public String createProject(@ModelAttribute("project")Project project){
+        service.createProject(project);
+        return "redirect:/projects";
     }
 
     @GetMapping("/new")
     public String showProjectForm(Model model){
         model.addAttribute("project", new Project());
-        return "createProject";
+        return "create-project";
     }
 
     @GetMapping("/edit/{id}")
     public String showEditForm(@PathVariable("id")int id, Model model){
         Project project = service.getProjectById(id).orElse(null);
         model.addAttribute("project", project);
-        return "editProject";
+        return "edit-project";
     }
 
     @PostMapping("/update/{id}")
     public String updateProject(@PathVariable("id") int id, @ModelAttribute("project") Project project){
         service.updateProject(id, project);
-        return "redirect:projects";
+        return "redirect:/projects";
     }
 
     @PostMapping("/delete/{id}")
     public String deleteProject(@PathVariable("id") int id){
         service.deleteProject(id);
-        return "redirect:projects";
+        return "redirect:/projects";
     }
 }
