8400234482b379f4b3186c37b3295497e0321ec9
AasifNazir786 <aasif.n.s786@gmail.com>
2024-10-05 15:29:15 +0530

    corrects the anchor tag link

--- MVC-SpringBoot/src/main/java/com/example/MVC_SpringBoot/controller/TaskController.java
+++ MVC-SpringBoot/src/main/java/com/example/MVC_SpringBoot/controller/TaskController.java
@@ -1,13 +1,18 @@
 package com.example.MVC_SpringBoot.controller;
 
-import com.example.MVC_SpringBoot.service.TaskService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import com.example.MVC_SpringBoot.model.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
+import com.example.MVC_SpringBoot.model.Task;
+import com.example.MVC_SpringBoot.service.TaskService;
 
 @Controller
 @RequestMapping("/tasks")
